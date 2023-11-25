beb375edeaf4c198fecbfda24d1d489b093e9c09
Leo Cui <basic.bear@outlook.com>
2023-11-25 16:31:50 -0500

    Initial commit

---
+++ test/market-data-ingest.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const MarketDataIngest = require('../lib/market-data-ingest-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/market-data-ingest-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new MarketDataIngest.MarketDataIngestStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
